c933338750b57184d3cb3c43cc8957eaf4cbd8a9
ArrayagoM <juanmartinarrayago@gmail.com>
2023-02-18 22:33:13 -0300

    Back

--- api/index.js
+++ api/index.js
@@ -17,10 +17,12 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { Country } = require('./src/db.js');
 const axios = require('axios');
+const  port  = process.env.PORT || 3001;
 
 // Inyectar los datos de la API en mi BD
 
@@ -56,8 +58,8 @@ const createDB = async () => {
 createDB();
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+conn.sync({ force: false }).then(() => {
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
